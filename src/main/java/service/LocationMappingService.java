4340093cc97be0a0ef79d070a4adfd495d4bff92
Sai Kumar Reddy <naga.kasa@gdn-commerce.com>
2024-05-17 00:06:32 +0530

    Added javadocs

--- src/main/java/service/LocationMappingService.java
+++ src/main/java/service/LocationMappingService.java
@@ -8,5 +8,10 @@ import java.util.Map;
 
 public interface LocationMappingService {
 
+    /**
+     * @param deliveryAgent : The User object of the deliveryAgent
+     * @param userIdMap : UserId to UserObject map
+     * @return the userId to the nodes map. Using one of the nodes we can traverse the whole graph.
+     */
     Map<String, Node> createLocationMapping(DeliveryAgent deliveryAgent, Map<String, User> userIdMap);
 }
